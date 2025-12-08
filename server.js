5fdd08b2cf7d8384fbe6d1cdc2b5d1553ea3a62d
Gabriel Buriti Geronimo <gburitigeronimo@gmail.com>
2025-12-08 17:19:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -50,7 +50,35 @@ app.post("/create-preference", async (req, res) => {
     }
 });
 
+// ROTA PIX - GERA QR CODE
+app.post("/create-pix", async (req, res) => {
+    try {
+        const items = req.body.items;
+        const total = items.reduce((t, i) => t + i.price * i.qty, 0);
+
+        const payment = await mercadopago.payment.create({
+            transaction_amount: total,
+            description: "Compra Shoopee20",
+            payment_method_id: "pix",
+            payer: {
+                email: "cliente@teste.com" // Pode trocar depois
+            }
+        });
+
+        return res.json({
+            qr: payment.body.point_of_interaction.transaction_data.qr_code_base64,
+            code: payment.body.point_of_interaction.transaction_data.qr_code
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: "Erro ao gerar Pix" });
+    }
+});
+
+
 // PORTA
 app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor rodando");
 });
+
