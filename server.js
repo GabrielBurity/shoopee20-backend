b3da958414ef3318c5c3c843c9459ce530e542a7
Gabriel Buriti Geronimo <gburitigeronimo@gmail.com>
2025-12-08 17:22:59 -0300

    Update server.js

--- server.js
+++ server.js
@@ -16,40 +16,6 @@ app.get("/", (req, res) => {
     res.send("Backend da Shoopee20 OK!");
 });
 
-// ROTA DE PAGAMENTO
-app.post("/create-preference", async (req, res) => {
-    try {
-        console.log("REQ:", req.body);
-
-        const preference = new Preference(client);
-
-        const result = await preference.create({
-            body: {
-                items: req.body.items.map(item => ({
-                    title: item.name,
-                    quantity: item.qty,
-                    unit_price: Number(item.price),
-                    currency_id: "BRL"
-                })),
-                auto_return: "approved",
-                back_urls: {
-                    success: "https://shoopee20.vercel.app/sucesso.html",
-                    failure: "https://shoopee20.vercel.app/erro.html",
-                    pending: "https://shoopee20.vercel.app/pendente.html"
-                }
-            }
-        });
-
-        console.log("RESULT:", result);
-
-        return res.json({ init_point: result.init_point });
-
-    } catch (error) {
-        console.error("Erro Mercado Pago:", error);
-        return res.status(500).json({ error: "Erro interno ao gerar pagamento" });
-    }
-});
-
 // ROTA PIX - GERA QR CODE
 app.post("/create-pix", async (req, res) => {
     try {
@@ -81,4 +47,5 @@ app.post("/create-pix", async (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor rodando");
 });
+
 
